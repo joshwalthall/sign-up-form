8752f78c85331e968d87f2ddaa02f465a9024922
Josh Walthall <josh.walthall@gmail.com>
2023-08-02 19:50:21 -0400

    Finalize Javascript logic for password field matching

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
 const pswdField = document.querySelector('#password');
 const pswdConfField = document.querySelector('#confirm-password');
+const pswdMessage = document.querySelector('#password-message');
 const createBtn = document.querySelector('#create-button');
 const form = document.querySelector('#form');
 
 const passwords = {new: '', conf: ''};
 
-pswdField.addEventListener('input', function() {updatePassword('new');});
-pswdConfField.addEventListener('input', function() {updatePassword('conf');});
-createBtn.addEventListener('click', function() {createAccount();});
+pswdField.addEventListener('change', function() {updatePassword('new', pswdField.value);});
+pswdConfField.addEventListener('change', function() {updatePassword('conf', pswdConfField.value);});
+// createBtn.addEventListener('click', function() {createAccount();});
 
 function updatePassword(pwdType, pwdText) {
     // Update the password or confirm password text to content of text field
+    console.log(`Updating ${pwdType} password to "${pwdText}"`)
     passwords[pwdType] = pwdText;
-    console.log(passwords[pwdType]);
+    matchPasswords();
 };
 
 function matchPasswords() {
     // Check if passwords match
-    let isMatch = false;
-    if (passwords.new === passwords.conf) {
-        isMatch = true;
-    };
-    return isMatch;
-};
-
-function createAccount() {
-    // Complain if passwords don't match, otherwise reset form
-    if (matchPasswords() === true) {
-        form.reset();
-        console.log("The passwords match somehow.");
-    } else if (matchPasswords() === false) {
-        console.log("Nice try, bucko.");
+    if (passwords.new != '' && passwords.conf != '' && passwords.new != passwords.conf) {
+        pswdMessage.textContent = "Passwords do not match";
+        createBtn.disabled = true;
+    } else if (passwords.new != '' && passwords.conf != '' && passwords.new === passwords.conf) {
+        pswdMessage.textContent = "";
+        createBtn.disabled = false;
+    } else if (passwords.new === '' && passwords.conf === '') {
+        pswdMessage.textContent = "";
+        createBtn.disabled = false;
     };
 };
